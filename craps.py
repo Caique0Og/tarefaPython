8c5f0132b010f6ffef4f40fe275d6118ad2327ce
Caique Rodrigues <142418237+Caique0Og@users.noreply.github.com>
2025-02-19 11:30:44 -0300

    Add files via upload

---
+++ craps.py
@@ -0,0 +1,34 @@
+import random
+Lista = []
+
+while True:
+    Jogar = str(input("Quer jogar um jogo?: "))
+    if Jogar == "sim"  or "SIM" or "Sim":
+        A = random.randint (1, 6)
+        B = random.randint (1, 6)
+        Resultado = A+B
+        Ponto = False
+
+        if Resultado == 7 and Ponto == False or Resultado == 11:
+            print("Ganhamo")
+            print(Resultado)
+            break
+            
+        elif Resultado == 7 and Ponto == True or Resultado <= 3 or Resultado == 12:
+            print ("Perdeu,")
+            print(Resultado)
+            break
+            
+        elif Resultado >= 4 and Resultado <= 10:
+            for Resultado in Lista:
+                print()
+                break   
+            Lista.append (Resultado)
+            print(Lista)
+            Ponto = True
+            print(Resultado)
+            
+        else:
+            print("complications, patrao")
+            break
+
